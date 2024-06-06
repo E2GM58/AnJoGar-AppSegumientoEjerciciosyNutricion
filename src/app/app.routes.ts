7c3c3a6d6aad8d901c9ac99c11b0eb163915c6ec
E2GM58 <101752844+E2GM58@users.noreply.github.com>
2024-06-06 15:26:10 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { PrincipalComponent } from './components/principal/principal.component';
 import{EjercicioComponent} from './components/ejercicio/ejercicio.component';
 import{IngresarEjercicioComponent} from './components/ingresar-ejercicio/ingresar-ejercicio.component';
 import{IngresarEjercicioFuerzaComponent} from './components/ingresar-ejercicio-fuerza/ingresar-ejercicio-fuerza.component';
+import { AlimentosComponent } from './components/alimentos/alimentos.component';
+import { AlimentosAddComponent } from './components/alimentos-add/alimentos-add.component';
 
 
 export const routes: Routes = [
@@ -10,5 +12,9 @@ export const routes: Routes = [
     { path: 'PaginaPrincipal', component: PrincipalComponent },
     { path: 'Ejercicio', component: EjercicioComponent },
     { path: 'IngresarE', component: IngresarEjercicioComponent },
-    { path: 'IngresarEF', component: IngresarEjercicioFuerzaComponent }
+    { path: 'IngresarEF', component: IngresarEjercicioFuerzaComponent },
+    { path: 'Alimentos', component: AlimentosComponent },
+    { path: 'AlimentosAdd', component: AlimentosAddComponent},
+    { path: 'AlimentosTotal', component: AlimentosAddComponent}
 ];
+
